a4733fff2702861f2bf95447fc726d3440938a75
William <wemz17@gmail.com>
2016-07-06 01:28:37 -0500

    comiendo las y sumando las pastillas

--- js/funciones.js
+++ js/funciones.js
@@ -18,7 +18,7 @@ var matriz = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 			  [0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0],
 			  [0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0],
 			  [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
-			  [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+			  [0,1,1,1,2,1,2,1,2,1,2,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
 			  [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
 			  [0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0],
 			  [0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0],
@@ -39,11 +39,27 @@ var matriz = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 			  [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
 			  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
 
-var lienzo,orientacion;			  
+var lienzo,orientacion,posicion;
+var contador;			  
 
 var fondo = {
 	fondoURL:"images/mapa.jpeg",
 	fondoOK:false,
+	x:0,
+	y:0,
+}
+
+var pastillas ={
+	pastillasURL:"images/pastilla.png",
+	pastillaOK:false,
+	x:0,
+	y:0,
+	cantidad:0,
+}
+
+var muros ={
+	murosURL:"images/muros.png",
+	murosOK:false,
 	x:0,
 	y:0,
 }
@@ -57,6 +73,7 @@ var player = {
 	playerUpOK:false,
 	playerDownURL:"images/pacmanDOWN.png",
 	playerDownOK:false,
+	
 	x:20,
 	y:400,
 	velocidad:20,
@@ -74,9 +91,19 @@ function inicio(){
 	var canvas = document.getElementById('cuadro');
 	lienzo = canvas.getContext('2d');
 
+	contador=document.getElementById('contador');
+
 	fondo.imagen = new Image();
 	fondo.imagen.src = fondo.fondoURL;
 	fondo.imagen.onload = confirmarFondo;
+
+	pastillas.imagen= new Image();
+	pastillas.imagen.src=pastillas.pastillasURL;
+	pastillas.imagen.onload=confirmarPastilla;
+
+	muros.imagen=new Image(),
+	muros.imagen.src=muros.murosURL;
+	muros.imagen.onload=confirmarMuros;
 
 	player.pacmanRIGHT = new Image();
 	player.pacmanRIGHT.src = player.playerRightURL;
@@ -94,6 +121,7 @@ function inicio(){
 	player.pacmanDOWN.src = player.playerDownURL;
 	player.pacmanDOWN.onload = confirmarPlayer;
 
+	
 	document.addEventListener("keydown",mover)
 
 }
@@ -104,16 +132,16 @@ function mover(press){
 	var comprobar = comprobarMapa(tecla);
 	console.log(comprobar);
 
-	if (tecla == movimiento.UP && comprobar == true){
+	if (tecla == movimiento.UP && comprobar == 1){
 		player.y -= player.velocidad; 
 	}
-	if (tecla == movimiento.DOWN && comprobar == true){
+	if (tecla == movimiento.DOWN && comprobar == 1){
 		player.y += player.velocidad;
 	}
-	if (tecla == movimiento.RIGHT && comprobar == true){
+	if (tecla == movimiento.RIGHT && comprobar == 1){
 		player.x += player.velocidad;
 	}	
-	if (tecla == movimiento.LEFT && comprobar == true){
+	if (tecla == movimiento.LEFT && comprobar == 1){
 		player.x -= player.velocidad;
 	}
 
@@ -122,7 +150,7 @@ function mover(press){
 	
 }
 
-// Muros de colision
+//  ---------   Muros de colision   ----------
 
 function comprobarMapa(undo){
 	var teclado=undo;
@@ -132,25 +160,40 @@ function comprobarMapa(undo){
 
   	console.log(x+","+y+"="+matriz[x][y]);
 
-	if ((teclado==movimiento.UP) && (matriz[x-1][y] == 1)){
-		//console.log("Estado de la Casilla a mover: "+matriz[y][x+1]);
-		return true;
+	if ((teclado==movimiento.UP) && (matriz[x-1][y] == 1)){	
+		return 1;
 	}
 	if ((teclado==movimiento.DOWN) && (matriz[x+1][y] == 1)){
-		return true;
+		return 1;
 	}
 	if ((teclado==movimiento.LEFT) && (matriz[x][y-1] == 1)){
-		return true;
+		return 1;
 	}
-	if ((teclado==movimiento.RIGHT) && (matriz[x][y+1] == 1)){
-		return true;
+	if ((teclado==movimiento.RIGHT) && (matriz[x][y+1] == 1) || (matriz[x][y+1] == 2)){
+		
+			if (matriz[x][y+1] == 2){
+				pastillas.cantidad--;
+				matriz[x][y+1]=1;
+
+		}
+		return 1;
 	}
- return false;
+ return 0;
+}
+ // ------------  CONFIRMACIONES DE IMAGENES -------------------
+function confirmarMuros(){
+	muros.murosOK=true;
+	dibujar();
 }
 
 
 function confirmarFondo(){
-	fondo.fondoOK=true;
+	fondo.fondoOK=1;
+	dibujar();
+}
+
+function confirmarPastilla(){
+	pastillas.pastillaOK=true;
 	dibujar();
 }
 
@@ -162,9 +205,56 @@ function confirmarPlayer(){
 	dibujar();
 }
 
+function scanMap(invoc){
+	var invocar=invoc;
+	var i=0;
+	var v=0;
+	if (invocar=="balones"){
+		var numero=0;
+		for ( i=0; i< 40;i++){
+			for ( v=0; v< 40;v++){
+				if (matriz[i][v]==2){
+					pastillas.x=i*20;
+					pastillas.y=v*20;
+					//alert("encontre pastilla por aqui en posicion  ("+ i+","+v +")");
+					//pastillas.cantidad=pastillas.cantidad;
+					numero++;
+					lienzo.drawImage(pastillas.imagen,pastillas.y,pastillas.x+30);
+
+				}
+			}
+		}
+		pastillas.cantidad=numero
+	}
+	if (invocar=="muros"){
+		for (i=0; i< 40;i++){
+			for ( v=0; v< 40;v++){
+				if (matriz[i][v]==0){
+					muros.x=i*20;
+					muros.y=v*20;
+						
+					lienzo.drawImage(muros.imagen,muros.y,muros.x);
+
+				}
+			}
+		}
+	}
+
+}
+
+
+// Funcion para Dibujar cuando ya todo esta comprobado
 function dibujar(){
+	
+
 	if (fondo.fondoOK == true){
 		lienzo.drawImage(fondo.imagen,fondo.x,fondo.y);
+	
+	}
+
+	if (muros.murosOK == true){
+			
+			
 	}
 
 	var estadoMov = player.pacmanRIGHT;
@@ -186,6 +276,12 @@ function dibujar(){
 					
 	lienzo.drawImage(estadoMov,player.x,player.y);
 	}
+	if (pastillas.pastillaOK == true){
+			
+		scanMap("balones");	
+	}
 	
+
+	contador.innerText=pastillas.cantidad;
 }
 
